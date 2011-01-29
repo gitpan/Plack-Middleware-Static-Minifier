c05a9c94408bb0c42564de7cef20218e26a2eaa2
Dai Okabayashi <bayashi@cpan.org>
2011-01-29 05:55:56 +0000

    Import of BAYASHI/Plack-Middleware-Static-Minifier-0.01 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-Static-Minifier
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BAYASHI/Plack-Middleware-Static-Minifier-0.01.tar.gz
    gitpan-cpan-author:       BAYASHI
    gitpan-cpan-maturity:     released

---
+++ lib/Plack/Middleware/Static/Minifier.pm
@@ -0,0 +1,86 @@
+package Plack::Middleware::Static::Minifier;
+use strict;
+use warnings;
+use Plack::Util;
+use parent 'Plack::Middleware::Static';
+use CSS::Minifier::XS qw//;
+use JavaScript::Minifier::XS qw//;
+
+our $VERSION = '0.01';
+
+sub call {
+    my $self = shift;
+    my $env  = shift;
+
+    my $res = $self->_handle_static($env);
+
+    if ($res && $res->[0] == 200) {
+        my $h = Plack::Util::headers($res->[1]);
+        if ( $h->get('Content-Type')
+                && $h->get('Content-Type') =~ m!/(css|javascript)! ) {
+            my $m = $1;
+            my $body; Plack::Util::foreach($res->[2], sub { $body .= $_[0] });
+            $res->[2] = ($m =~ m!^css!)
+                      ? [ CSS::Minifier::XS::minify($body) ]
+                      : [ JavaScript::Minifier::XS::minify($body) ];
+            $h->set('Content-Length', length $res->[2][0]);
+        }
+    }
+
+    if ($res && not ($self->pass_through and $res->[0] == 404)) {
+        return $res;
+    }
+
+    return $self->app->($env);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Static::Minifier - serves static files and minify CSS and JavaScript
+
+
+=head1 SYNOPSIS
+
+    use Plack::Builder;
+    builder {
+        enable "Plack::Middleware::Static::Minifier",
+            path => qr{^/(js|css|images)/},
+            root => './htdocs/';
+        $app;
+    };
+
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Static::Minifier serves static files with Plack and minify CSS and JavaScript. This module is the subclass of Plack::Middleware::Static.
+
+See L<Plack::Middleware::Static> for more detail.
+
+
+=head1 REPOSITORY
+
+Plack::Middleware::Static::Minifier is hosted on github
+<http://github.com/bayashi/Plack-Middleware-Static-Minifier>
+
+
+=head1 AUTHOR
+
+Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
+
+
+=head1 SEE ALSO
+
+L<Plack::Middleware::Static>, L<CSS::Minifier::XS>, L<JavaScript::Minifier::XS>
+L<Plack::Middleware>, L<Plack>
+
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
